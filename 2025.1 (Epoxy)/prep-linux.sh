464678266f555efa5e4063608b5ece1144a0daac
Kris Celmer <krzysztof_celmer@hotmail.com>
2025-07-24 20:06:28 +0200

    Update prep-linux.sh

--- 2025.1 (Epoxy)/prep-linux.sh
+++ 2025.1 (Epoxy)/prep-linux.sh
@@ -56,7 +56,7 @@ echo "<---"
 
 echo "---> Updating and installing basic packages"
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu-kvm virt-manager parted
+sudo apt install -y bridge-utils cpu-checker qemu-kvm parted
 sudo apt install -y python3-dev python3-venv git libffi-dev gcc libssl-dev libdbus-glib-1-dev vim nano net-tools htop dnsutils yq
 echo "<---"
 
