acc5bfa74420d3b458abfab429ad0ffc5e538b54
Kris Celmer <krzysztof_celmer@hotmail.com>
2025-08-10 12:12:10 +0200

    changes

--- 2025.1 (Epoxy)/scripts/utils/sec10-show-errors.sh
+++ 2025.1 (Epoxy)/scripts/utils/sec10-show-errors.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 # Quick health scan for current project
 
 echo "Project: ${OS_PROJECT_NAME:-UNKNOWN}"
@@ -7,6 +7,6 @@ echo "---------------------------------------"
 
 res=(server volume image network router port)
 for r in "${res[@]}"; do
-  echo "Checking \$r resources for errors:"
+  echo "Checking $r resources for errors:"
   openstack $r list --long -c ID -c name -c status | awk -v rname=$r 'NR>3 && $0 !~ /(ACTIVE|UP|AVAILABLE|active)/ {print rname":",$0}'
 done
